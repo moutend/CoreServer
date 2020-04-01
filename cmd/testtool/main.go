0a02454f65afaeb5ecb7f3d4188d75d62ba0d7d9
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-04-01 09:41:58 +0900

    Chore

--- cmd/testtool/main.go
+++ cmd/testtool/main.go
@@ -42,10 +42,7 @@ func run(args []string) error {
 
 	foo.SetIUIEventHandler(func(eventId int64, pInterface uintptr) int64 {
 		fmt.Printf("@@@received %x\n", pInterface)
-		time.Sleep(11 * time.Second)
-		uiae := &com.IUIAutomationElement{}
-		vtblPtr := *(*uintptr)(unsafe.Pointer(pInterface))
-		uiae.RawVTable = (*interface{})(unsafe.Pointer(vtblPtr))
+		uiae := (*com.IUIAutomationElement)(unsafe.Pointer(pInterface))
 
 		rect, err := uiae.CurrentBoundingRectangle()
 		fmt.Println("@@@bounding", rect, err)
