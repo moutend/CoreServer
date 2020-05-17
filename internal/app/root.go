7ae65c5fbace525ee19c5a55fa8190f517d91900
Yoshiyuki Koyanagi <moutend@gmail.com>
2020-05-18 06:52:53 +0900

    Chore

--- internal/app/root.go
+++ internal/app/root.go
@@ -83,7 +83,7 @@ func rootRunE(cmd *cobra.Command, args []string) error {
 		localizedControlType, _ := e.CurrentLocalizedControlType()
 
 		go http.Post("http://127.0.0.1:7902/v1/audio", "application/json", bytes.NewBufferString(
-			fmt.Sprintf(`{"isForcePush":true,"commands":[{"type":1,"sfxIndex":9},{"type":3,"textToSpeech":%q},{"type":2,"sleepDuration":7500},{"type":3,"textToSpeech":%q}]}`,
+			fmt.Sprintf(`{"isForcePush":true,"commands":[{"type":1,"sfxIndex":9},{"type":3,"textToSpeech":%q},{"type":2,"sleepDuration":750},{"type":3,"textToSpeech":%q}]}`,
 				name,
 				localizedControlType,
 			)))
